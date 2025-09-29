48e436495f002b4a7877cf5d31d9bde9760ae550
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-09-29 18:49:06 +0900

    install and use Physx

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 Physxを試してみるレポジトリ
 
 ## 参考にしたURL
-- [GitHub - NVIDIA PhysX SDK 4.1](https://github.com/NVIDIAGameWorks/PhysX)
+- [GitHub - NVIDIA PhysX](https://github.com/NVIDIAGameWorks/PhysX)
 - [Zenn - 物理エンジンPhysXを触ってみよう](https://zenn.dev/hama1080/books/c107c7c945018a)
 - [GitHub - PhysicsEnginePrimer](https://github.com/hama1080/PhysicsEnginePrimer)
+
+## メモ
+- [4系](https://github.com/NVIDIAGameWorks/PhysX)はWindows、Linux、Android、Mac、iOSに対応している。<br>しかし、[vcpkgでfind_packageに対応していない](https://github.com/microsoft/vcpkg/issues/10233)みたい。
+- [5系](https://github.com/NVIDIAGameWorks/PhysX)はWindows、Linuxに対応している。 ※当レポジトリでは5系を使用している。<br>[非公式でMac、iOS、Androidに対応したfork](https://github.com/o3de/PhysX)が存在するが、vcpkgでパッケージが提供されているのは公式版だけみたい。
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,87 @@
+#include <PxPhysicsAPI.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+using namespace std;
+using namespace physx;
 
-    return EXIT_SUCCESS;
+PxDefaultAllocator gAllocator;
+PxDefaultErrorCallback gErrorCallback;
+PxFoundation* gFoundation = NULL;
+PxPhysics* gPhysics = NULL;
+PxDefaultCpuDispatcher* gDispatcher = NULL;
+PxScene* gScene = NULL;
+PxPvd* gPvd = NULL;
+
+// PhysXの初期化
+void initPhysics() {
+    gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+
+    // PVDの設定
+    gPvd = PxCreatePvd(*gFoundation);
+    PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+    gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+    gPhysics =
+        PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+    PxInitExtensions(*gPhysics, gPvd);
+
+    // Sceneの作成
+    PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+    sceneDesc.gravity = PxVec3(0.0f, -9.8f, 0.0f);
+    gDispatcher = PxDefaultCpuDispatcherCreate(0);
+    sceneDesc.cpuDispatcher = gDispatcher;
+    sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+    gScene = gPhysics->createScene(sceneDesc);
+
+    // PVDの設定
+    PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+    if (pvdClient) {
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+    }
+}
+
+// Dynamic Rigidbodyの作成
+PxRigidDynamic* createDynamic(const PxTransform& t,
+                              const PxGeometry& geometry,
+                              PxMaterial& material,
+                              PxReal density = 10.0f) {
+    PxRigidDynamic* rigid_dynamic = PxCreateDynamic(*gPhysics, t, geometry, material, density);
+    gScene->addActor(*rigid_dynamic);
+    return rigid_dynamic;
+}
+
+// シミュレーションステップを進める
+void stepPhysics() {
+    const PxReal kElapsedTime = 1.0f / 60.0f; // 60Hz
+    gScene->simulate(kElapsedTime);
+    gScene->fetchResults(true);
+}
+
+int main(void) {
+    initPhysics();
+    cout << "PhysXHelloWorld" << endl;
+    cout << "Start simulation" << endl;
+
+    const PxU32 kMaxSimulationStep = 100;
+
+    // 静摩擦係数、動摩擦係数、反発係数の順
+    PxMaterial* const kMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+    // 半径1mの球を高さ10mから落とす
+    PxRigidDynamic* sphere = createDynamic(
+        PxTransform((PxVec3(0.0f, 10.0f, 0.0f))), PxSphereGeometry(1.0f), *kMaterial);
+
+    for (PxU32 i = 0; i != kMaxSimulationStep; i++) {
+        PxVec3 p = sphere->getGlobalPose().p;
+        cout << "Step: " << i << ", Position: (" << p.x << ", " << p.y << ", " << p.z << ")"
+             << endl;
+        stepPhysics();
+    }
+
+    cout << "End simulation" << endl;
+    int tmp;
+    cin >> tmp;
+    return 0;
 }
